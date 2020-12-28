d523204ef973eddbb882f4161b751f9f581842fe
d3x0r <d3x0r@no-reply.github.com>
2020-12-27 22:33:05 -0800

    Add popup menu function

--- README.md
+++ README.md
@@ -33,3 +33,28 @@ popup.appendChild( createElement( "canvas" ) ); // add something to draw into on
 Generally popups work like VERY simple GUI widgets...
 some of the popups.makeButton sort of methods return just a simple html element.
 
+
+
+|popup methods| arguments/type  | description |
+|----|---|---|
+|	defaultDrag | true | option toggle whether to enable mouse dragging on frames |
+|	autoRaise | true |  option to toggle whether clicks on frames auto raise them to top |
+|	create | (caption [, parent])  | create a new blank popup with specified caption ; if caption is an empty string, no title caption is shown.  |
+|	simpleForm | (title, question, defaultValue, ok, cancelCb )  | create a simple yesno form with a title, and a string question; and a text input field for user input  |
+|	simpleNotice | ( title, question, ok, cancel )  |  Create just a popup notice with Ok, and optional Cancel button.   |
+|        list | (parentList,toString) | Creates a list with a specified tostring method for elements.   |
+|        makeCheckbox | (form, o, text, field )  |  make a checkmark button, which is bound to object 'o' and member 'field'.  The title is shown next to the checkbox.  |
+|        makeNameInput | (form, o, text, field ) | make a static text display, with a button to click to edit the text   |
+ |       makeTextInput |(form,o,text,field) |  make a text input with a lable next to it   |
+|        makeTextField |(form,o,text,field)  | make a text display with a label next to it (like input but readonly)   |
+ |       makeButton | (parentElement,text, callback) | adds a button; is 2 divs nested and styled.  Callback is called when the button is clicked/touched. |
+|	setClass| (element.class) | Set a class in className |
+|	toggleClass| (element,class) | if a class is in a class, remove it, else add it |
+|	clearClass| (element,class) | remove a class from className |
+
+
+### Lists
+
+Drag and drop nested items in lists.
+
+
--- popups.mjs
+++ popups.mjs
@@ -32,6 +32,7 @@ const popups = {
 	setClass: setClass,
 	toggleClass: toggleClass,
 	clearClass:clearClass,
+	createMenu : createPopup,
 }
 
 var popupTracker;
@@ -757,6 +758,96 @@ function makeNameInput( form, input, text ){
 			el.className += " " + cn;
 		}
 	}
+
+
+//--------------------------- Quick Popup Menu System ------------------------------
+
+var mouseCatcher = document.getElementById( "mouseCatcher" ) || document.createElement( "div" );
+if( !mouseCatcher ) {
+	if( mouseCatcher.name !== "mouseCatcher" ) {
+		document.body.appendChild( mouseCatcher )
+	}
+	
+}
+var topMenu;
+mouseCatcher.addEventListener( "click", (evt)=>{
+	mouseCatcher.style.visibility = "hidden";
+	if( topMenu )
+		topMenu.hide( true );
+} );
+
+function createPopup() {
+	var menu = {
+		items: [],
+		parent : null,
+		container : document.createElement( "div" ),
+		board : null,
+		// hMenu, MF_STRING, MNU_ADDNEURON, ("Add &Neuron") );
+		appendItem( _flags, value, text ) {
+			if( _flags & MF_SEPARATOR) {
+				var newItem = document.createElement( "HR" );
+				this.container.appendChild( newItem );
+
+			}else {
+				var newItem = document.createElement( "A" );
+				var newItemBR = document.createElement( "BR" );
+				this.container.appendChild( newItem );
+				this.container.appendChild( newItemBR );
+				var flags = _flags;
+				newItem.value = value;
+				newItem.textContent = text;
+				newItem.className = "popup";
+				if( flags & MF_POPUP ) {
+					value.parent = this;
+					newItem.addEventListener( "mouseover", (evt)=>{
+						var r = newItem.getBoundingClientRect();
+						console.log( "Item is clicked show that.", evt.target.value, evt.clientX, evt.clientY );
+
+						newItem.value.show( this.board, evt.clientX, r.top - 10, this.cb );
+					} );
+					newItem.addEventListener( "mouseout", (evt)=>{
+						var r = newItem.getBoundingClientRect();
+						console.log( "Item is clicked show that.", evt.target.value, evt.clientX, r.top );
+						if( evt.toElement !== newItem.value.container )		
+							newItem.value.hide();
+					} );
+				} else
+					newItem.addEventListener( "click", (evt)=>{
+						this.cb( evt.target.value );
+						console.log( "Item is clicked.", evt.target.value );
+						this.hide( true );
+					} );
+			}
+		},
+		hide( all ) {
+			this.container.style.visibility = "hidden";
+			if( this.parent ) {
+				if( all )
+					this.parent.hide( all );
+			} else {
+				mouseCatcher.style.visibility = "hide"
+			}
+		},
+		show( board, x, y, cb ) {
+			this.board = board;
+			this.cb = cb;
+			mouseCatcher.style.visibility = "visible"
+			this.container.style.visibility = "inherit";
+			this.container.style.left = x;
+			this.container.style.top = y;
+		},
+		reset() {
+			console.log( "hide everything?" );	
+		}
+	};
+
+	mouseCatcher.appendChild( menu.container );
+	menu.container.className = "popup";
+	menu.container.style.zIndex = 50;
+
+	return menu;
+}
+
 
 
 export {popups};
