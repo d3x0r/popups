543176bca33b33075579279a671c772b6f6bfea0
d3x0r <d3x0r@no-reply.github.com>
2021-05-30 12:11:04 -0700

    Add fillFromURL method to be able to populate a control with HTML

--- popups.mjs
+++ popups.mjs
@@ -45,6 +45,7 @@ const popups = {
 	createMenu : createPopupMenu,
         makeLoginForm: makeLoginForm,
         makeWindowManager : makeWindowManager,
+        fillFromURL : fillFromURL,
 }
 
 const globalMouseState = {
@@ -1986,6 +1987,47 @@ function makeWindowManager() {
                 console.log( "this should remove this whole construct from the page" );
             }
         }
+}
+
+function fillFromURL(popup, url) {
+
+    fetch(url).then(response => {
+	response.text().then( (text)=>{
+                popup.divContent.innerHTML = text;
+		nodeScriptReplace(popup.divContent);
+	} );
+       })
+
+	return popup;
+
+	function nodeScriptReplace(node) {
+                if ( nodeScriptIs(node) === true ) {
+                        node.parentNode.replaceChild( nodeScriptClone(node) , node );
+                }
+                else {
+                        var i = -1, children = node.childNodes;
+                        while ( ++i < children.length ) {
+                              nodeScriptReplace( children[i] );
+                        }
+                }
+
+        	return node;
+	}
+	function nodeScriptClone(node){
+                var script  = document.createElement("script");
+                script.text = node.innerHTML;
+
+                var i = -1, attrs = node.attributes, attr;
+                while ( ++i < attrs.length ) {
+                      script.setAttribute( (attr = attrs[i]).name, attr.value );
+                }
+        	return script;
+	}
+
+	function nodeScriptIs(node) {
+        	return node.tagName === 'SCRIPT';
+	}
+
 }
 
 
