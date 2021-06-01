d748bfcf792972cc3c1d15594a722ed1a23d60c6
Cliff Christianson <cliffchristianson@gmail.com>
2021-06-01 01:52:51 +0000

    Add support utils for $ and % conversions; forward promise from fillFromURL

--- popups.mjs
+++ popups.mjs
@@ -19,8 +19,89 @@ popup.divContent  // insert frame content here
 //import {JSOX} from "jsox";
 //import {JSOX} from "../../jsox/lib/jsox.mjs";
 const utils = globalThis.utils || {
-	to$(s) { return "$"+s;},
+    ROUND_DOWN:1,
+    ROUND_UP :2,
+    ROUND_NATURAL:3,
+    // --------- These need to go into utils or something
+    to$(val,rounder) {
+        function pad(val, n) {
+            if( val.length < n) {
+                val = '00000'.substr(0,n-val.length)+val;
+            }
+            return val;
+        }
+        var digits = Math.log10(val) - 2;
+        var n;
+        var r = '';
+        var c = (val/100)|0;
+        var cnts;
+        if( cnts = val % 100 ) {
+            if( rounder === 1 ) {
+                if( val < 0 )
+                    val -= 1;
+                else   
+                    val += 1;
+            }else if( rounder === 2 ){
+                if( val < 0 )
+                    val += 1;
+                    else
+                    val -=1 ;
+            }else if( rounder === 3 ) {
+                if( val < 0 )
+                    if( cnts >= 50 )
+                        val -= 1;
+                    else
+                        val += 1;
+                else
+                    if( cnts >= 50 )
+                        val += 1;
+                    else
+                        val -= 1;
+            }
+            else
+                r = '.' + pad((val%100).toString(),2);
+        }
+        if( digits >= 3 ) {
+            for( n = 0; n <= digits-3; n += 3) {
+                r = "," + pad(((c%1000)|0).toString(),3) + r;
+                c = (c / 1000)|0;
+            }
+        }
+        r = '$' + (c%1000) + r;
+        return r;
+    },
+
+    toD($) {
+        if( "string" !== typeof $ )
+            $ = $.toString();
+        if( $[0] === '$' )
+            $ = $.substr(1);
+        //   throw new Error( "NOT A DOLLAR AMOUNT" );
+        var i = $.indexOf('.' );
+        if( i >= 0 && $.length-i > 2 ) {
+            var trunc = $.split(',' ).join('').split('.');
+            trunc[trunc.length-1] = trunc[trunc.length-1].substr(0,2);
+            return Number( trunc.join('') );
+
+        } else if( i >= 0 && $.length-i == 3 )
+            return Number( $.split(',' ).join('').split('.').join('') );
+        else if( i >= 0 && $.length-i == 2 )
+            return Number( $.split(',' ).join('').split('.').join('') ) * 10;
+            return (Number( $.split(',' ).join('') ) * 100)|0;
+    },
+
+    toP(p) {
+        if( "string" !== typeof p )
+            p = p.toString();
+        return p + "%";
+    },
+    fromP(p){
+        p = p.split('%').join('');
+        return Number(p);        
+    }
 }
+
+
 const localStorage = globalThis.localStorage;
 
 const popups = {
@@ -231,7 +312,10 @@ class Popup {
         divClose = document.createElement( "div" );
 	popup = this;
 
-	constructor(caption_,parent,forContent) {
+	constructor(caption_,parent,opts) {
+            	const suffix = opts?.suffix ||'';
+		// make popup from control.
+		const forContent = opts?.from;
                 if( forContent ) {
                     this.divFrame = forContent;
                     this.divContent = null;
@@ -308,6 +392,7 @@ class Popup {
 			this.divFrame.style.display = "none";
 		}
 		show() {
+                    	this.raise();
 			this.divFrame.style.display = "";
 			//popupTracker.raise( this );
 
@@ -1003,7 +1088,7 @@ function makeNameInput( form, input, value, text ){
 	binder.className = "fieldUnit";
 	form.appendChild(binder );
 	binder.appendChild( textLabel );
-	binder.appendChild( text );
+	binder.appendChild( textOutput );
 	binder.appendChild( buttonRename );
 	//binder.appendChild( document.createElement( "br" ) );
 	return {
@@ -1991,14 +2076,18 @@ function makeWindowManager() {
 
 function fillFromURL(popup, url) {
 
-    fetch(url).then(response => {
-	response.text().then( (text)=>{
+    return fetch(url).then(response => {
+	return response.text().then( (text)=>{
                 popup.divContent.innerHTML = text;
 		nodeScriptReplace(popup.divContent);
+		return popup;
+                return new Promise( (res,rej)=>{
+
+		       // popup;
+                } );
 	} );
        })
 
-	return popup;
 
 	function nodeScriptReplace(node) {
                 if ( nodeScriptIs(node) === true ) {
