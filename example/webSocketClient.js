297ded2e3d85a48f565b4e37bd27a99acb4c8e9e
d3x0r <d3x0r@no-reply.github.com>
2021-06-14 23:09:13 -0700

    Option simplified load

--- example/webSocketClient.js
+++ example/webSocketClient.js
@@ -1,7 +1,8 @@
 
-import {popups,AlertForm} from "/node_modules/@d3x0r/popups/popups.mjs"
 
+//import {popups,AlertForm} from "/node_modules/@d3x0r/popups/popups.mjs"
 //import {JSOX} from "/node_modules/jsox/lib/jsox.mjs"
+import {popups,AlertForm} from ",,/popups.mjs"
 const JSOX = JSON;
 
 let isGuestLogin = false;
